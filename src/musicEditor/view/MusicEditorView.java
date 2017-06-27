0e0fa42999a1d9ec0b10a8dda75982dcab2dda4f
Primer81 <gwlarkham@gmail.com>
2017-06-27 19:00:26 -0400

    Added a constructor to the view and some setters as well as skeletoms for future methods.

--- src/musicEditor/view/MusicEditorView.java
+++ src/musicEditor/view/MusicEditorView.java
@@ -6,5 +6,42 @@ import musicEditor.music.MusicSheet;
  * Represents the view for the music editor.
  */
 public class MusicEditorView implements IMusicEditorView {
-  MusicSheet sheet;
+  private JPanel editorPanel;
+  private JPanel pianoPanel;
+  
+  private MusicComposition composition;
+  private MusicTracker tracker;
+  private MusicPlayer player;
+  
+  /**
+   * Constructs new MusicEditorView with default fields.
+   */
+  public MusicEditorView() {
+    this.editorPanel = new EditorPanel();
+    this.pianoPanel = new PianoPanel();
+    
+    this.composition = new MusicComposition();
+    this.tracker = new MusicTracker();
+    this.player = new MusicPlayer();
+  }
+  
+  public void setComposition(MusicComposition composition) {
+    this.composition = composition;
+  }
+
+  public void setTracker(MusicTracker tracker) {
+    this.tracker = tracker;
+  }
+
+  public void setPlayer(MusicPlayer player) {
+    this.player = player;
+  }
+
+  public void initialize() {
+    JFrame frame = new Jframe();
+  }
+  
+  public void update() {
+    
+  }
 }
